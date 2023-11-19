cbf54fb30cc9d659c250f7096974e119dcf7af25
Hossam Samir <hossamsamir@outlook.com>
2023-11-19 16:00:16 +0200

    update component state

--- src/App.js
+++ src/App.js
@@ -3,53 +3,43 @@ import "./App.css";
 import ListContacts from "./ListContacts";
 import { Component } from "react";
 
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg",
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg",
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-];
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id != contact.id;
+      }),
+    }));
+  };
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
       </div>
     );
   }
